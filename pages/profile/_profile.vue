1f3c99d020b9701408a7fc955b686b60ddb6700e
aminahmadi <aminahmadi.i2007@gmail.com>
2024-09-23 23:27:38 +0330

    Initial commit

---
+++ pages/profile/_profile.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <div class="p-5"></div>
+        <section class=" px-4 py-2 container my-4 d-flex justify-content-center ">
+            <div class="row bg-F7F7F7 p-4 rounded-4 border-gr">
+                <div class="col-12 d-flex flex-column my-2">
+                    <div class=" d-flex justify-content-center justify-content-sm-start">
+                        <img src="/person-dash.jpg" alt="" width="130px" class="rounded-pill">
+                    </div>
+                <div class="row mt-5">
+                    <div class="col">
+                        <p class="m-0 h2">amin ahmadi</p>
+                    </div>
+                    <div class="col" dir="rtl"><img src="/icons8-edit-30.png" alt=""></div>
+                </div>
+            </div>
+            <hr>
+            <div class="col-12">
+                <div class="row d-flex justify-content-center px-2">
+                    <div class="w-50 p-1">
+                        <p class="h5 my-1">birthdate</p>
+                        <p class="h6 m-0 c-316070">00/00/00</p>
+                    </div>
+                    <div class="w-50  p-1">
+                        <p class="h5 my-1">phone number</p>
+                        <p class="h6 m-0 c-316070">+98 000 00 0 0 0 0 </p>
+                    </div>
+                    <div class="w-50  p-1">
+                        <p class="h5 my-1">national code</p>
+                        <p class="h6 m-0 c-316070">000 000 000</p>
+                    </div>
+                    <div class="w-50 p-1">
+                        <p class="h5 my-1">birthdate</p>
+                        <p class="h6 m-0 c-316070">00/00/00</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 c-316070 h5 mt-4">
+                <p class="text-black">Address</p>
+                <div class="h6 d-flex my-2">
+                    1)<p class="mx-1">Lorem ipsum dolor sit amet, consectetur</p>
+                </div>
+                <div class="h6 d-flex">
+                    1)<p class="mx-1">Lorem ipsum dolor sit amet, consectetur</p>
+                </div>
+            </div>
+            <div class="col-12 mt-4">
+                <button
+              class="rounded-2 fs-6 py-2 px-3 bg-00214F border-none text-white"
+            >
+               cart history <img src="/icons8-right-35.png" alt="" />
+            </button>
+            </div>
+            </div>
+        </section>
+    </div>
+</template>
+<style>
+    .c-316070{
+        color: #095D88;
+    }
+    .bg-00214F{
+        background-color: #0a3f8a;
+    }
+    .bg-F7F7F7{
+        background-color: #F7F7F7;
+    }
+    .border-gr{
+        border: 2px solid rgb(151, 128, 128);
+    }
+</style>
---
+++ pages/profile/edit.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+        <div class="p-5"></div>
+        <section class=" px-4 py-2 container my-4 d-flex justify-content-center ">
+            <div class="row bg-F7F7F7 p-4 rounded-4 border-gr w-100">
+                <div class="col-12">
+                    <div class=" d-flex justify-content-center justify-content-sm-start">
+                        <img src="/person-dash.jpg" alt="" width="130px" class="rounded-pill">
+                    </div>
+                </div>
+                <div class="col-12 mt-4">
+                    <div class="row px-1">
+                        <div class="col-12 col-md-8 col-lg-5 col-xl-4 mt-3">
+                            <div class="row">
+                            <div class="col-5 p-0 mr-5">
+                                <p class="m-0">First Name</p>
+                                <input
+                                type="number"
+                                class="w-100  mt-1 px-3 py-2 border-00A693 rounded-2"
+                                placeholder="amin"
+                                />
+                            </div>
+                            <div class="col-5 p-0 mr-5">
+                                <p class="m-0">Last Name</p>
+                                <input
+                                type="number"
+                                class="w-100 mt-1 px-3 py-2 border-00A693 rounded-2"
+                                placeholder="ahmadi"
+                                />
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col-12 mt-3">
+                            <div class="row d-flex justify-content-center">
+                                <div class="col-5 col-md p-0 mr-5">
+                                    <p class="m-0">birthdate</p>
+                                    <input
+                                    type="number"
+                                    class="w-100  mt-1 px-3 py-2 border-00A693 rounded-2"
+                                    placeholder="00/00/00<"
+                                    />
+                                </div>
+                                <div class="col-5 col-md p-0 mr-5">
+                                    <p class="m-0">number</p>
+                                    <input
+                                    type="number"
+                                    class="w-100  mt-1 px-3 py-2 border-00A693 rounded-2"
+                                    placeholder="+98 000 00 0 0 0 0 "
+                                    />
+                                </div>
+                                <div class="col-5 col-md p-0 mr-5">
+                                    <p class="m-0">national code</p>
+                                    <input
+                                    type="number"
+                                    class="w-100  mt-1 px-3 py-2 border-00A693 rounded-2"
+                                    placeholder="8888"
+                                    />
+                                </div>
+                                <div class="col-5 col-md p-0 mr-5">
+                                    <p class="m-0">password</p>
+                                    <input
+                                    type="number"
+                                    class="w-100  mt-1 px-3 py-2 border-00A693 rounded-2"
+                                    placeholder="*******"
+                                    />
+                                </div>
+                                <div class="col-11 col-md p-0 mr-5">
+                                    <p class="m-0">Email</p>
+                                    <input
+                                    type="number"
+                                    class="w-100  mt-1 px-3 py-2 border-00A693 rounded-2"
+                                    placeholder="@gmail.com"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mt-4">
+                <button
+              class="rounded-2 fs-6 py-2 px-3 bg-00214F border-none text-white"
+            >
+               confirm <img src="/icons8-right-35.png" alt="" />
+            </button>
+            </div>
+            </div>
+        </section>
+    </div>
+</template>
+<style>
+    .c-316070{
+        color: #095D88;
+    }
+    .bg-00214F{
+        background-color: #0a3f8a;
+    }
+    .bg-F7F7F7{
+        background-color: #F7F7F7;
+    }
+    .border-gr{
+        border: 2px solid rgb(151, 128, 128);
+    }
+    .border-00A693 {
+  border: 2px solid #00a693;
+    }
+</style>
